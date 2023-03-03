4993bc0aa6347117be1ef203985dc784a709a739
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-03 22:38:56 +0300

    Доработал класс SignUpFormValidator

--- src/pages/SignUpPage/components/SignUpForm/useSignUpForm.ts
+++ src/pages/SignUpPage/components/SignUpForm/useSignUpForm.ts
@@ -1,16 +1,16 @@
 import { useEffect, useRef } from "react";
-import { SignUpFormController } from "./SignUpFormController";
+import { SignUpFormValidator } from "./SignUpFormValidator";
 
 export const useSignUpForm = (
   onSubmit: (email: string, password: string) => void
 ) => {
   const refRoot = useRef<HTMLDivElement>(null);
-  const refSignUpFormController = useRef<SignUpFormController | null>(null);
+  const refSignUpFormValidator = useRef<SignUpFormValidator | null>(null);
 
   useEffect(() => {
     const root = refRoot.current;
-    if (root !== null && refSignUpFormController.current === null) {
-      refSignUpFormController.current = new SignUpFormController({
+    if (root !== null && refSignUpFormValidator.current === null) {
+      refSignUpFormValidator.current = new SignUpFormValidator({
         root,
         onSubmit,
       });
