2808312f42cf2287ee1796f3987843627c6992c4
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-05 20:45:49 +0300

    Добавил hash-роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { constants } from "./constants";
 import { CartPage } from "./pages/CartPage/CartPage";
 import { MainPage } from "./pages/MainPage/MainPage";
@@ -7,7 +7,7 @@ import { SignUpPage } from "./pages/SignUpPage/SignUpPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={constants.routes.CartPage.path} element={<CartPage />} />
         <Route
@@ -20,7 +20,7 @@ function App() {
         />
         <Route path="*" element={<MainPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/components/Header/components/HeaderMobile/HeaderMobile.tsx
+++ src/components/Header/components/HeaderMobile/HeaderMobile.tsx
@@ -1,54 +1,64 @@
 import { Logo } from "../../../Logo/Logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { constants } from "../../../../constants";
 import { useHeaderMobile } from "./useHeaderMobile";
 import { observer } from "mobx-react-lite";
-import { store, UserStatus } from "../../../../store";
 import { NavLink } from "react-router-dom";
 import { LogOut } from "../../../LogOut/LogOut";
 
 import s from "./HeaderMobile.module.css";
+import { constants } from "../../../../constants";
+
+interface RouteInfo {
+  path: string;
+  title: string;
+  showInNavBarForGuest: boolean;
+  showInNavBarForLoggedIn: boolean;
+}
 
 interface Props {
+  routes: RouteInfo[];
+  isUserLoggedIn: boolean;
   className?: string;
 }
 
-export const HeaderMobile: React.FC<Props> = observer(({ className }) => {
-  const isLoggedIn = store.getUserState().userStatus === UserStatus.LoggedIn;
-  const { isOpened, handleClick } = useHeaderMobile();
-  const routes = Object.values(constants.routes).filter(
-    ({ enableForGuest, enableForLoggedIn }) =>
-      isLoggedIn ? enableForLoggedIn : enableForGuest
-  );
+export const HeaderMobile: React.FC<Props> = observer(
+  ({ className, routes, isUserLoggedIn }) => {
+    const { isOpened, handleClick } = useHeaderMobile();
+    const { logoutText } = constants.Header;
 
-  return (
-    <div className={className}>
-      <header className={s.root}>
-        <div className={s.container}>
-          <Logo />
-          <button onClick={handleClick} className={s.openNavButton}>
-            <FontAwesomeIcon color="white" icon={faBars} />
-          </button>
-        </div>
-      </header>
-      {isOpened && (
-        <ul className={s.list}>
-          {routes.map(({ path, title }) => (
-            <li className={s.li} key={path}>
-              <NavLink
-                className={({ isActive }) => (isActive ? s.linkActive : s.link)}
-                to={path}
-              >
-                {title}
-              </NavLink>
+    return (
+      <div className={className}>
+        <header className={s.root}>
+          <div className={s.container}>
+            <Logo />
+            <button onClick={handleClick} className={s.openNavButton}>
+              <FontAwesomeIcon color="white" icon={faBars} />
+            </button>
+          </div>
+        </header>
+        {isOpened && (
+          <ul className={s.list}>
+            {routes.map(({ path, title }) => (
+              <li className={s.li} key={path}>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? s.linkActive : s.link
+                  }
+                  to={path}
+                >
+                  {title}
+                </NavLink>
+              </li>
+            ))}
+            <li className={s.li}>
+              {isUserLoggedIn && (
+                <LogOut className={s.logout} text={logoutText} />
+              )}
             </li>
-          ))}
-          <li className={s.li}>
-            {isLoggedIn && <LogOut className={s.logout} />}
-          </li>
-        </ul>
-      )}
-    </div>
-  );
-});
+          </ul>
+        )}
+      </div>
+    );
+  }
+);
