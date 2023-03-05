7732cf1dc4fdaa3208e10b38face334215d90ee8
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-05 12:18:51 +0300

    Добавил стили для страниц авторизации и регистрации

--- src/pages/SignInPage/components/SignInForm/SignInForm.tsx
+++ src/pages/SignInPage/components/SignInForm/SignInForm.tsx
@@ -17,9 +17,9 @@ export const SignInForm: React.FC<Props> = ({ className, onSubmit }) => {
   return (
     <div ref={refRoot} className={cn(className, s.root)}>
       <label className={s.label}>{emailInput.label}</label>
-      <input type="email" id={emailInput.id} />
+      <input className={s.input} type="email" id={emailInput.id} />
       <label className={s.label}>{passwordInput.label}</label>
-      <input type="password" id={passwordInput.id} />
+      <input className={s.input} type="password" id={passwordInput.id} />
       {state.errorText && <div className={s.error}>{state.errorText}</div>}
       <button
         onClick={handleSubmit}
