fa138a48bcb90ac39ad152f0bddd9bd8535262e0
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-07 23:07:39 +0300

    Добавил компонент для выбора нескольких вариантов

--- src/pages/MainPage/components/SearchProductsForm/SearchProductsForm.tsx
+++ src/pages/MainPage/components/SearchProductsForm/SearchProductsForm.tsx
@@ -1,29 +1,11 @@
-import { Select } from "../../../../components/Select/Select";
-import { ProductCategory, ProductsSortType } from "../../../../constants/enums";
-import { texts } from "../../../../constants/texts";
+import { SearchProductsInput } from "../SearchProductsInput/SearchProductsInput";
 
 import s from "./SearchProductsForm.module.css";
 
 export const SearchProductsForm = () => {
   return (
-    <form className={s.root}>
-      <ul className={s.searchParams}>
-        <li className={s.selectSearchParam}>
-          <Select<ProductCategory>
-            onChange={(option) => alert(option)}
-            options={texts.ProductCategorySelect.variants}
-            title={texts.ProductCategorySelect.title}
-          />
-        </li>
-        <li className={s.selectSearchParam}>
-          <Select<ProductsSortType>
-            onChange={(option) => alert(option)}
-            options={texts.ProductSortTypeSelect.variants}
-            title={texts.ProductSortTypeSelect.title}
-          />
-        </li>
-      </ul>
-      <input className={s.searchInput} />
-    </form>
+    <div className={s.root}>
+      <SearchProductsInput className={s.searchInput} />
+    </div>
   );
 };
