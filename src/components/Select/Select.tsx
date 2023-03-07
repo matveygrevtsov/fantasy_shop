6bcbb8a515ab0aa918771cf574e60878e7869883
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-07 22:34:41 +0300

    Добавил компонент для выбора нескольких вариантов

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,30 +1,47 @@
 import cn from "classnames";
+import { useSelect } from "./useSelect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 import s from "./Select.module.css";
 
 interface Props<T> {
   options: Record<string, string>;
   title: string;
-  onChange: (option: T) => void;
+  onChange: (option?: T) => void;
   className?: string;
 }
 
 export function Select<T>({ options, title, onChange, className }: Props<T>) {
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value as unknown as T;
-    onChange(value);
-  };
+  const { state, handleSelect, handleOpenCloseClick } = useSelect<T>(onChange);
+  const optionsListClassName = state.isOpened
+    ? s.optionsListOpened
+    : s.optionsListClosed;
 
   return (
-    <select onChange={handleChange} className={cn(s.root, className)}>
-      <option disabled selected>
+    <div className={cn(s.root, className)}>
+      <div onClick={handleOpenCloseClick} className={s.top}>
         {title}
-      </option>
-      {Object.entries(options).map(([key, value]) => (
-        <option value={key} key={key}>
-          {value}
-        </option>
-      ))}
-    </select>
+        <FontAwesomeIcon
+          className={s.openOptionsListIcon}
+          icon={faChevronDown}
+        />
+      </div>
+      <ul className={optionsListClassName}>
+        {Object.entries(options).map(([key, value]) => {
+          const option = key as unknown as T;
+          return (
+            <li key={key} onClick={() => handleSelect(option)}>
+              <input
+                className={s.checkbox}
+                type="checkbox"
+                checked={option === state.activeOption}
+              />
+              <span>{value}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
