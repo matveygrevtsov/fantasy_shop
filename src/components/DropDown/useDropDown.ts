d98d628df05e2ea451b9602aa4283d4c4c051c05
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-08 21:16:15 +0300

    Добавил закрытие фильтров поиска по клику вне фильтра

--- src/components/DropDown/useDropDown.ts
+++ src/components/DropDown/useDropDown.ts
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useClickOutsideListener } from "../../hooks/useClickOutsideListener";
 
 export function useDropDown() {
   const [opened, setIsOpened] = useState<boolean>(false);
-  const handleClick = () => setIsOpened((prevState) => !prevState);
+  const handleOpen = () => setIsOpened(true);
+  const handleClose = () => setIsOpened(false);
+  const { ref } = useClickOutsideListener<HTMLDivElement>(handleClose);
 
-  return { opened, handleClick };
+  return { opened, ref, handleOpen };
 }
