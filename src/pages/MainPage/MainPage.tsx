bdddf34edb3730f54a98aa6233dad6e0d7da1b79
matveygrevtsov <matveygrevcov@gmail.com>
2023-03-09 20:09:18 +0300

    Добавил возможность скачивания списка продуктов после чтения searchparams

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,31 @@
 import { SearchProductsForm } from "./components/SearchProductsForm/SearchProductsForm";
-import { useMainPage } from "./useMainPage";
+import { StateStatus, useMainPage } from "./useMainPage";
+import { Preloader } from "../../components/Preloader/Preloader";
+import { ProductsList } from "../../components/ProductsList/ProductsList";
 
 import s from "./MainPage.module.css";
 
 export const MainPage = () => {
-  const { handleSubmit } = useMainPage();
+  const { state, handleSubmit } = useMainPage();
 
   return (
     <div className={s.root}>
-      <SearchProductsForm onSubmit={handleSubmit} />
+      <SearchProductsForm
+        className={s.searchProductsForm}
+        onSubmit={handleSubmit}
+      />
+      {state.status === StateStatus.Loading && (
+        <Preloader className={s.preloader} />
+      )}
+      {state.status === StateStatus.Error && (
+        <h2>
+          При скачивании списка продуктов произошла ошибка. Пожалуйста,
+          повторите позже.
+        </h2>
+      )}
+      {state.status === StateStatus.Success && (
+        <ProductsList products={state.products} />
+      )}
     </div>
   );
 };
--- src/pages/MainPage/useMainPage.ts
+++ src/pages/MainPage/useMainPage.ts
@@ -1,15 +1,87 @@
 import { SearchProductsFormState } from "./types";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { decodeBase64, encodeBase64 } from "../../utils/base64";
+import { ProductCategory, ProductsSortType } from "../../constants/enums";
+import { useEffect, useState } from "react";
+import { Product } from "../../types";
+
+export enum StateStatus {
+  Loading = "Loading",
+  Error = "Error",
+  Success = "Success",
+}
+
+type State =
+  | {
+      status: StateStatus.Loading | StateStatus.Error;
+    }
+  | {
+      status: StateStatus.Success;
+      products: Product[];
+    };
 
 export function useMainPage() {
+  const [state, setState] = useState<State>({
+    status: StateStatus.Loading,
+  });
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   function handleSubmit(
     searchProductsFormState: SearchProductsFormState
   ): void {
-    console.log(searchProductsFormState);
+    navigate(`?searchParams=${encodeBase64(searchProductsFormState)}`);
   }
 
-  return { handleSubmit };
+  useEffect(() => {
+    setState({
+      status: StateStatus.Loading,
+    });
+    mapSearchParamsToProducts(searchParams.get("searchParams") || "").then(
+      (products) => {
+        setState({
+          status: StateStatus.Success,
+          products,
+        });
+      },
+      () => setState({ status: StateStatus.Error })
+    );
+  }, [searchParams, mapSearchParamsToProducts, setSearchParams]);
+
+  return { state, handleSubmit };
+}
+
+async function mapSearchParamsToProducts(
+  searchParams: string
+): Promise<Product[]> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [];
+}
+
+function isSearchProductsFormState(obj: any): obj is SearchProductsFormState {
+  if (!obj || typeof obj !== "object") return false;
+  if (typeof obj.searchString !== "string") return false;
+  if (!obj.searchParams || typeof obj.searchParams !== "object") return false;
+  if (
+    typeof obj.searchParams.productCategories !== "object" ||
+    !Array.isArray(obj.searchParams.productCategories)
+  ) {
+    return false;
+  }
+
+  if (
+    !Object.values(ProductsSortType).includes(obj.searchParams.productsSortType)
+  ) {
+    return false;
+  }
+
+  if (
+    obj.searchParams.productCategories.some(
+      (category: any) => !Object.values(ProductCategory).includes(category)
+    )
+  ) {
+    return false;
+  }
+
+  return true;
 }
