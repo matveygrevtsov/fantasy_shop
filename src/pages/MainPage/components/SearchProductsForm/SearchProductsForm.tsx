6baa13d0d358b512965bc7981a73895cf61809dd
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-03-11 13:54:07 +0300

    Переделал форму поиска продуктов через react-hook-form (#48)
    * Переделал форму поиска продуктов через react-hook-form
    
    * Переделал форму поиска продуктов через react-hook-form
    
    ---------
    
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/pages/MainPage/components/SearchProductsForm/SearchProductsForm.tsx
+++ src/pages/MainPage/components/SearchProductsForm/SearchProductsForm.tsx
@@ -4,6 +4,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { DropDown } from "../../../../components/DropDown/DropDown";
 import { texts } from "../../../../constants/texts";
 import { SearchProductsParams } from "../../../../types";
+import { Controller } from "react-hook-form";
 import Select from "react-select";
 
 import s from "./SearchProductsForm.module.css";
@@ -38,26 +39,49 @@ export function SearchProductsForm({ className, onSubmit }: Props) {
             <label className={s.label}>
               {texts.ProductsSearchForm.ProductCategorySelect.title}
             </label>
-            <Select
-              // {...register("productsCategories")}
-              placeholder={texts.ProductsSearchForm.ProductCategorySelect.title}
-              isMulti={true}
-              options={texts.ProductsSearchForm.ProductCategorySelect.options}
+            <Controller
+              name="productsCategories"
+              control={control}
+              defaultValue={[]}
+              render={({ field: { onChange, value, ref } }) => (
+                <Select
+                  ref={ref}
+                  value={texts.ProductsSearchForm.ProductCategorySelect.options.filter(
+                    (option) => value.includes(option.value)
+                  )}
+                  onChange={(options) =>
+                    onChange(options.map((option) => option.value))
+                  }
+                  options={
+                    texts.ProductsSearchForm.ProductCategorySelect.options
+                  }
+                  isMulti
+                />
+              )}
             />
           </li>
           <li className={s.searchParam}>
             <label className={s.label}>
               {texts.ProductsSearchForm.ProductSortTypeSelect.title}
             </label>
-            <Select
-              // {...register("productsSortType")}
+            <Controller
+              name="productsSortType"
+              control={control}
               defaultValue={
-                texts.ProductsSearchForm.ProductSortTypeSelect.defaultOption
+                texts.ProductsSearchForm.ProductSortTypeSelect.options[0].value
               }
-              options={[
-                texts.ProductsSearchForm.ProductSortTypeSelect.defaultOption,
-                ...texts.ProductsSearchForm.ProductSortTypeSelect.options,
-              ]}
+              render={({ field: { onChange, value, ref } }) => (
+                <Select
+                  options={
+                    texts.ProductsSearchForm.ProductSortTypeSelect.options
+                  }
+                  value={texts.ProductsSearchForm.ProductSortTypeSelect.options.find(
+                    (option) => option.value === value
+                  )}
+                  onChange={(option) => onChange(option?.value)}
+                  ref={ref}
+                />
+              )}
             />
           </li>
         </ul>
