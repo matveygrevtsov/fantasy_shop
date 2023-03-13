d2c00ecfa81ef930aa73f7c1997b33c00796bad8
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-03-13 15:13:20 +0300

    Добавил подсветку Dropzone при драгге (#53)
    * Добавил подсветку Dropzone при драгге
    
    * Добавил подсветку Dropzone при драгге
    
    ---------
    
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/components/ImagesUploader/ImagesUploader.tsx
+++ src/components/ImagesUploader/ImagesUploader.tsx
@@ -1,26 +1,49 @@
 import Dropzone from "react-dropzone";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileImage, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faFileImage, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { texts } from "../../constants/texts";
 import { useImagesUploader } from "./useImagesUploader";
 import cn from "classnames";
 
 import s from "./ImagesUploader.module.css";
 
+export enum DropzoneStatus {
+  DragEnter = "DragEnter",
+  DragLeave = "DragLeave",
+}
+
 interface Props {
   onSelect: (images: File[]) => void;
   className?: string;
 }
 
+const CONFIG = {
+  accept: { "image/*": [] },
+};
+
 export function ImagesUploader({ onSelect, className }: Props) {
-  const { handleSelect, images, handleRemoveImage } =
-    useImagesUploader(onSelect);
+  const {
+    state,
+    handleSelect,
+    handleRemoveImage,
+    handleDragEnter,
+    handleDragLeave,
+  } = useImagesUploader(onSelect);
+  const dropZoneClassName =
+    state.status === DropzoneStatus.DragEnter ? s.dropZoneActive : s.dropZone;
 
   return (
     <div className={cn(s.root, className)}>
-      <Dropzone onDrop={handleSelect}>
+      <Dropzone
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDropRejected={console.log}
+        onError={console.log}
+        onDrop={handleSelect}
+        {...CONFIG}
+      >
         {({ getRootProps, getInputProps }) => (
-          <div className={s.dropZone} {...getRootProps()}>
+          <div className={dropZoneClassName} {...getRootProps()}>
             <input {...getInputProps()} />
             <FontAwesomeIcon icon={faFileImage} className={s.uploadIcon} />
             <p>{texts.ImagesUploader.placeholder}</p>
@@ -28,7 +51,7 @@ export function ImagesUploader({ onSelect, className }: Props) {
         )}
       </Dropzone>
       <ul className={s.images}>
-        {images.map(({ src }) => (
+        {state.images.map(({ src }) => (
           <li key={src} className={s.imageContainer}>
             <img
               src={src}
@@ -43,7 +66,7 @@ export function ImagesUploader({ onSelect, className }: Props) {
             >
               <FontAwesomeIcon
                 className={s.removeImageIcon}
-                icon={faCircleXmark}
+                icon={faTrashCan}
               />
             </button>
           </li>
